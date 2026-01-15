69bb3a49d6dd7d23ee6751906adfadc1fec89374
Krane & Mora <kranemora@gmail.com>
2026-01-15 13:20:58 -0500

    Fix featured image sync when replaced or removed

--- src/edit.js
+++ src/edit.js
@@ -19,7 +19,7 @@ import {
 } from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
 import buildBackgroundStyle from './utils/buildBackgroundStyle';
-import { useState, useRef } from '@wordpress/element';
+import { useState, useRef, useEffect } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 
 
@@ -35,11 +35,44 @@ const edit = ({ attributes, setAttributes, clientId }) => {
         setAttributes({ blockId: clientId.slice(0, 8) });
     }
 
-    const featuredImage = useSelect((select) => {
-        const featuredId = select('core/editor').getEditedPostAttribute('featured_media');
-        if (!featuredId) return null;
-        return select('core').getEntityRecord('postType', 'attachment', featuredId);
-    }, []);
+    const featuredImageId = useSelect(
+        (select) =>
+            select('core/editor').getEditedPostAttribute('featured_media')
+    );
+
+    const featuredImage = useSelect(
+        (select) => {
+            if (!featuredImageId) return null;
+            return select('core').getEntityRecord(
+                'postType',
+                'attachment',
+                featuredImageId
+            );
+        },
+        [featuredImageId]
+    );
+
+    useEffect(() => {
+        if (!backgroundImage) return;
+        if (backgroundImage.source !== 'featured') return;
+
+        // Si se borra la featured
+        if (!featuredImageId) {
+            setAttributes({ backgroundImage: null });
+            return;
+        }
+
+        // Si se reemplaza la featured
+        if (featuredImage && backgroundImage.id !== featuredImage.id) {
+            setAttributes({
+                backgroundImage: {
+                    ...backgroundImage,
+                    id: featuredImage.id,
+                    url: featuredImage.source_url,
+                }
+            });
+        }
+    }, [featuredImageId, featuredImage, backgroundImage]);
 
     const hasFeaturedImage = useSelect(
         (select) => {
@@ -132,6 +165,7 @@ const edit = ({ attributes, setAttributes, clientId }) => {
                     repeat: 'no-repeat',
                     size: 'cover',
                     attachment: 'scroll',
+                    source: 'featured'
                 }
             });
         }
@@ -280,7 +314,7 @@ const edit = ({ attributes, setAttributes, clientId }) => {
 
                                                     <MenuItem
                                                         icon={
-                                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M19 3H5c-.6 0-1 .4-1 1v7c0 .5.4 1 1 1h14c.5 0 1-.4 1-1V4c0-.6-.4-1-1-1zM5.5 10.5v-.4l1.8-1.3 1.3.8c.3.2.7.2.9-.1L11 8.1l2.4 2.4H5.5zm13 0h-2.9l-4-4c-.3-.3-.8-.3-1.1 0L8.9 8l-1.2-.8c-.3-.2-.6-.2-.9 0l-1.3 1V4.5h13v6zM4 20h9v-1.5H4V20zm0-4h16v-1.5H4V16z"></path></svg>                                                        }
+                                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M19 3H5c-.6 0-1 .4-1 1v7c0 .5.4 1 1 1h14c.5 0 1-.4 1-1V4c0-.6-.4-1-1-1zM5.5 10.5v-.4l1.8-1.3 1.3.8c.3.2.7.2.9-.1L11 8.1l2.4 2.4H5.5zm13 0h-2.9l-4-4c-.3-.3-.8-.3-1.1 0L8.9 8l-1.2-.8c-.3-.2-.6-.2-.9 0l-1.3 1V4.5h13v6zM4 20h9v-1.5H4V20zm0-4h16v-1.5H4V16z"></path></svg>}
                                                         isFullWidth
                                                         onClick={() => {
                                                             openFeaturedImage();
