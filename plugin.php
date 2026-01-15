41e7f650e647e0d7560008fed8188c1cc26923da
Krane & Mora <kranemora@gmail.com>
2026-01-15 12:05:43 -0500

    Enhance responsive background images with density support and filter cleanup

--- plugin.php
+++ plugin.php
@@ -13,56 +13,10 @@
  * Domain Path: /languages
  */
 
-function kmwp_coverlays_render_block( $attributes, $content ) {
-
-    if (
-        empty( $attributes['blockId'] ) ||
-        empty( $attributes['backgroundImage']['id'] )
-    ) {
-        return $content;
-    }
-
-    $image_id = (int) $attributes['backgroundImage']['id'];
-
-    $block_id = esc_attr( $attributes['blockId'] );
-    $image_id = (int) $attributes['backgroundImage']['id'];
-
-    $selector = ".coverlays-block-{$block_id}";
-
-    $layers = $attributes['layers'] ?? [];
-    $overlay_layers = kmwp_coverlays_build_overlay_layers( $layers );
-    
-    $breakpoints = kmwp_coverlays_get_default_breakpoints();
-    $breakpoints = apply_filters( 'kmwp_coverlays_breakpoints', $breakpoints, $attributes );
-
-    $sources = kmwp_coverlays_resolve_image_sources( $image_id, $breakpoints );
-    $sources = apply_filters( 'kmwp_coverlays_image_sizes', $sources, $attributes ); 
-
-    if ( empty( $sources ) ) {
-        return $content;
-    }
-
-    $background_image = $attributes['backgroundImage'];
-    $background_properties = kmwp_coverlays_build_background_properties( $background_image );
-
-    $css = kmwp_coverlays_generate_background_css(
-        $selector,
-        $sources,
-        $overlay_layers,
-        $background_properties
-    );
-
-    $css = apply_filters( 'kmwp_coverlays_css', $css, $attributes );
-
-    wp_add_inline_style( 'kmwp-coverlays-style', $css );
-
-    return $content;
-}
-
 function kmwp_coverlays_register_block()
 {
     register_block_type(
-        __DIR__.'/build',
+        __DIR__ . '/build',
         [
             'render_callback' => 'kmwp_coverlays_render_block',
         ]
@@ -71,79 +25,108 @@ function kmwp_coverlays_register_block()
 
 add_action('init', 'kmwp_coverlays_register_block');
 
-function kmwp_coverlays_register_style() {
+function kmwp_coverlays_register_style()
+{
     wp_register_style(
         'kmwp-coverlays-style',
         false,
         [],
         '1.0.0'
     );
-
 }
-add_action( 'init', 'kmwp_coverlays_register_style' );
+add_action('init', 'kmwp_coverlays_register_style');
 
-function kmwp_coverlays_enqueue_style_if_needed() {
+function kmwp_coverlays_enqueue_style_if_needed()
+{
 
-    if ( ! is_singular() ) {
+    if (! is_singular()) {
         return;
     }
 
     global $post;
 
-    if ( ! $post || ! has_block( 'kmwp/coverlays', $post ) ) {
+    if (! $post || ! has_block('kmwp/coverlays', $post)) {
         return;
     }
 
-    wp_enqueue_style( 'kmwp-coverlays-style' );
+    wp_enqueue_style('kmwp-coverlays-style');
 }
-add_action( 'wp_enqueue_scripts', 'kmwp_coverlays_enqueue_style_if_needed' );
+add_action('wp_enqueue_scripts', 'kmwp_coverlays_enqueue_style_if_needed');
 
 
-function kmwp_coverlays_get_default_breakpoints() {
-    return [
-        0    => 'medium',
-        600  => 'medium_large',
-        1024 => 'large',
-        1536 => '1536x1536',
-        2048 => 'full',
-    ];
-}
+function kmwp_coverlays_render_block($attributes, $content)
+{
 
-function kmwp_coverlays_resolve_image_sources( int $attachment_id, array $breakpoints = [] ): array {
-
-
-    if ( empty( $breakpoints ) ) {
-        $breakpoints = kmwp_coverlays_get_default_breakpoints();
+    if (
+        empty($attributes['blockId']) ||
+        empty($attributes['backgroundImage']['id'])
+    ) {
+        return $content;
     }
+
+    $block_id = esc_attr($attributes['blockId']);
+    $image_id = (int) $attributes['backgroundImage']['id'];
+
+    $selector = ".coverlays-block-{$block_id}";
+
+    $layers = $attributes['layers'] ?? [];
+    $overlay_layers = kmwp_coverlays_build_overlay_layers($layers);
+
+    $sources = kmwp_coverlays_resolve_image_sources($image_id);
+    $sources = apply_filters('kmwp_coverlays_image_sizes', $sources, $attributes);
+
+    if (empty($sources)) {
+        return $content;
+    }
+
+    $background_image = $attributes['backgroundImage'];
+    $background_properties = kmwp_coverlays_build_background_properties($background_image);
+
+    $css = kmwp_coverlays_generate_background_css(
+        $selector,
+        $sources,
+        $overlay_layers,
+        $background_properties
+    );
+
+    $css = apply_filters('kmwp_coverlays_css', $css, $attributes);
+
+    wp_add_inline_style('kmwp-coverlays-style', $css);
+
+    return $content;
+}
+
+function kmwp_coverlays_resolve_image_sources(int $attachment_id): array
+{
 
     $sources = [];
+    $attachment_metadata = wp_get_attachment_metadata($attachment_id);
+    $uploads = wp_upload_dir();
+    $base_url = trailingslashit($uploads['baseurl']);
+    $base_dir  = dirname($attachment_metadata['file']) . '/';
 
-    foreach ( $breakpoints as $max_width => $size ) {
-
-        $image = wp_get_attachment_image_src( $attachment_id, $size );
-
-        if ( ! $image ) {
-            continue;
+    if (! empty($attachment_metadata['sizes'])) {
+        foreach ($attachment_metadata['sizes'] as $metadata) {
+            $sources[$metadata['width']] = $base_url . $base_dir . $metadata['file'];
         }
-
-        $sources[ $max_width ] = $image[0];
     }
-
+    $sources[$attachment_metadata['width']] = $base_url . $attachment_metadata['file'];
     return $sources;
 }
 
-function kmwp_coverlays_build_overlay_layers( array $layers ): array {
+function kmwp_coverlays_build_overlay_layers(array $layers): array
+{
 
     $background_layers = [];
 
-    foreach ( $layers as $layer ) {
+    foreach ($layers as $layer) {
 
-        if ( ! empty( $layer['gradient'] ) ) {
+        if (! empty($layer['gradient'])) {
             $background_layers[] = $layer['gradient'];
             continue;
         }
 
-        if ( ! empty( $layer['color'] ) ) {
+        if (! empty($layer['color'])) {
             $color = $layer['color'];
             $background_layers[] = "linear-gradient({$color}, {$color})";
         }
@@ -152,13 +135,14 @@ function kmwp_coverlays_build_overlay_layers( array $layers ): array {
     return $background_layers;
 }
 
-function kmwp_coverlays_build_background_properties( array $background_image ): string {
+function kmwp_coverlays_build_background_properties(array $background_image): string
+{
 
     $size = $background_image['size'] ?? 'cover';
     $repeat = $background_image['repeat'] ?? 'no-repeat';
     $attachment = $background_image['attachment'] ?? 'scroll';
 
-    if ( ! empty( $background_image['focalPoint'] ) ) {
+    if (! empty($background_image['focalPoint'])) {
         $x = $background_image['focalPoint']['x'] * 100;
         $y = $background_image['focalPoint']['y'] * 100;
         $position = "{$x}% {$y}%";
@@ -166,12 +150,12 @@ function kmwp_coverlays_build_background_properties( array $background_image ): 
         $position = '50% 50%';
     }
 
-    return implode( ' ', [
+    return implode(' ', [
         "background-size: {$size};",
         "background-repeat: {$repeat};",
         "background-position: {$position};",
         "background-attachment: {$attachment};",
-    ] );
+    ]);
 }
 
 function kmwp_coverlays_generate_background_css(
@@ -181,40 +165,93 @@ function kmwp_coverlays_generate_background_css(
     string $background_properties
 ): string {
 
-    ksort( $sources );
+    ksort($sources);
 
     $css = '';
-    $is_first = true;
 
-    foreach ( $sources as $min_width => $url ) {
+    /**
+     * Regla base: propiedades invariantes
+     */
+    $css .= "{$selector} {\n";
+    $css .= "    {$background_properties}\n";
+    $css .= "}\n";
+
+    $is_first = true;
+    $min_width = 0;
+
+    foreach ($sources as $max_width => $url) {
 
         $layers = array_merge(
             $overlay_layers,
-            [ "url('{$url}')" ]
+            ["url('{$url}')"]
         );
 
-        $background_image = implode( ', ', $layers );
+        $background_image = implode(', ', $layers);
 
-        $rule = "{$selector} {
-            background-image: {$background_image};
-            {$background_properties}
-        }";
+        $rule = "{$selector} {\n";
+        $rule .= "    background-image: {$background_image};\n";
+        $rule .= "}";
 
-        if ( $is_first ) {
+        if ($is_first) {
             $css .= $rule . "\n";
             $is_first = false;
-            continue;
+        } else {
+            $css .= "@media (min-width: {$min_width}px) {\n";
+            $css .= "  {$rule}\n";
+            $css .= "}\n";
         }
 
-        $css .= "@media (min-width: {$min_width}px) {\n";
-        $css .= "  {$rule}\n";
-        $css .= "}\n";
+        foreach ([2, 3] as $density) {
+
+            $target_width = $max_width * $density;
+            $hires_url = null;
+            $last_url  = null;
+
+            foreach ($sources as $w => $u) {
+                $last_url = $u;
+                if ($w >= $target_width) {
+                    $hires_url = $u;
+                    break;
+                }
+            }
+
+            // fallback: usar la más grande disponible
+            if (! $hires_url) {
+                $hires_url = $last_url;
+            }
+
+            $layers_hd = array_merge(
+                $overlay_layers,
+                ["url('{$hires_url}')"]
+            );
+
+            $background_hd = implode(', ', $layers_hd);
+
+            $rule_hd = "{$selector} {\n";
+            $rule_hd .= "    background-image: {$background_hd};\n";
+            $rule_hd .= "}";
+
+            $dpi = $density * 96;
+
+            if ($min_width === 0) {
+                $css .= "@media (min-resolution: {$dpi}dpi), (-webkit-min-device-pixel-ratio: {$density}) {\n";
+            } else {
+                $css .= "@media (min-width: {$min_width}px) and (min-resolution: {$dpi}dpi),\n";
+                $css .= "       (min-width: {$min_width}px) and (-webkit-min-device-pixel-ratio: {$density}) {\n";
+            }
+
+            $css .= "  {$rule_hd}\n";
+            $css .= "}\n";
+        }
+
+        $min_width = $max_width + 1;
     }
 
     return $css;
 }
 
-function kmwp_coverlays_load_textdomain() {
+function kmwp_coverlays_load_textdomain()
+{
     load_plugin_textdomain(
         'kmwp-coverlays',
         false,
@@ -223,11 +260,12 @@ function kmwp_coverlays_load_textdomain() {
 }
 add_action('plugins_loaded', 'kmwp_coverlays_load_textdomain');
 
-function kmwp_coverlays_set_script_translations() {
+function kmwp_coverlays_set_script_translations()
+{
     wp_set_script_translations(
         'kmwp-coverlays-editor-script',
         'kmwp-coverlays',
-        plugin_dir_path( __FILE__ ) . 'languages'
+        plugin_dir_path(__FILE__) . 'languages'
     );
 }
-add_action( 'init', 'kmwp_coverlays_set_script_translations' );
+add_action('init', 'kmwp_coverlays_set_script_translations');
